23d6d3279f4d9e9c9c07250f68be691ad491cbc1
whtslv <vladlee.2001@mail.ru>
2020-10-23 23:06:13 +0600

    third

--- timecontrol/settings.py
+++ timecontrol/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 LOGOUT_URL = 'index'
 LOGIN_URL = 'login/'
 LOGIN_REDIRECT_URL = 'index'
